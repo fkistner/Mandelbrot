37c936b60f8acd724d615e3e49a06cc947679091
Florian Kistner <mail@fkistner.com>
2016-03-17 09:37:29 +0100

    Initial

---
+++ Mandelbrot/MandelbrotView.h
@@ -0,0 +1,13 @@
+//
+//  MandelbrotScrollView.h
+//  Mandelbrot
+//
+//  Created by Florian Kistner on 15/03/16.
+//  Copyright © 2016 Florian Kistner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MandelbrotView : UIView
+
+@end
---
+++ Mandelbrot/MandelbrotView.m
@@ -0,0 +1,13 @@
+//
+//  MandelbrotScrollView.m
+//  Mandelbrot
+//
+//  Created by Florian Kistner on 15/03/16.
+//  Copyright © 2016 Florian Kistner. All rights reserved.
+//
+
+#import "MandelbrotView.h"
+
+@implementation MandelbrotView
+
+@end
